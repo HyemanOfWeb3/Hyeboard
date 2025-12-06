694abe477eb3cd6128517e7ecfb0d3511d6cbbb1
MOMI <user@example.com>
2025-12-06 20:33:19 +0100

    prepared for deploymnent

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -12,19 +13,39 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+// const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 //middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Adjust the origin as needed
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // Adjust the origin as needed
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 //app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+// app.use(express.static(path.join(path.resolve(), "frontend", "dist")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(path.resolve(), "frontend", "dist", "index.html"));
+// });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+//connect to database and start the server
 
 connectDB().then(() => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+
+const BASE_URL = import.meta.env.MODE === "production"
+  ? "http://localhost:5001/api" : "/api" ;
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
-  
+  baseURL: BASE_URL ,
 });
 
 
