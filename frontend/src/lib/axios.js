369cf145eb84c6e8edd621fb4c94ef562ce7098d
MOMI <user@example.com>
2026-01-15 17:09:11 +0100

    Fix axios baseURL to correctly route to /api/notes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "production" ? "" : "http://localhost:5001";
+  import.meta.env.MODE === "production" ? "/api" : "http://localhost:5001/api";
 const api = axios.create({
   baseURL: BASE_URL,
 });
